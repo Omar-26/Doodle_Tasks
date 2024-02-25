d76a7235831fb72c82d3f91353ae4d6048583ea4
Omar Ashraf <eng.omar.ashraf26@gmail.com>
2024-02-25 19:54:36 +0200

    Updated To-Do App

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'view/screens/add task/add_task_page.dart';
+import 'view/screens/home/home_page.dart';
+import 'package:todo_app/constants/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return MaterialApp(
+      // scrollBehavior: MaterialScrollBehavior(),
+      debugShowCheckedModeBanner: false,
+      title: 'To-Do App',
+      home: const HomePage(),
+      routes: {
+        '/homepage': (context) => const HomePage(),
+        '/addtaskpage': (context) => const AddTaskPage(),
+      },
+    );
+  }
+}
